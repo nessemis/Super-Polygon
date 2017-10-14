aaa68ea691f89f4bb1a674c0fe3dcd76bf2131d7
Doycie <imcody97@gmail.com>
2017-10-14 12:54:23 +0200

    Improved polygon

--- src/View.hs
+++ src/View.hs
@@ -10,7 +10,6 @@ view = return . viewPure
 
 
 viewPure :: GameState -> Picture
-viewPure gstate = pictures [ color white $ polygon [(10,0),(3.0,9.5),(-8.1,5.9),(-8.1,-5.9), ( 3.1 ,-9.5)] , case infoToShow gstate of
   ShowNothing   -> blank
   ShowANumber n -> color green (text (show n))
   ShowAChar   c -> color green (text [c])]
