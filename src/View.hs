e5a8aec1372c8ede3f796afa9d814417aba80b59
Doycie <imcody97@gmail.com>
2017-10-14 12:50:35 +0200

    polygon :)

--- src/View.hs
+++ src/View.hs
@@ -8,8 +8,9 @@ import Model
 view :: GameState -> IO Picture
 view = return . viewPure
 
+
 viewPure :: GameState -> Picture
-viewPure gstate = case infoToShow gstate of
+viewPure gstate = pictures [ color white $ polygon [(10,0),(3.0,9.5),(-8.1,5.9),(-8.1,-5.9), ( 3.1 ,-9.5)] , case infoToShow gstate of
   ShowNothing   -> blank
   ShowANumber n -> color green (text (show n))
-  ShowAChar   c -> color green (text [c])
+  ShowAChar   c -> color green (text [c])]
